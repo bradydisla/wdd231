b19b9a13da948b4aad770318e2575d92993fe27d
bradydisla <bradydue@gmail.com>
2026-09-05 18:16:06 -0400

    #1

--- WEEK1/scripts/date.js
+++
@@ -1,5 +0,0 @@
-const currentYear = document.getElementById('currentyear');
-currentYear.textContent = new Date().getFullYear();
-
-const lastModified = document.getElementById('lastModified');
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
--- WEEK1/scripts/navigation.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById('menu-btn');
-const nav = document.getElementById('primary-nav');
-
-menuBtn.addEventListener('click', () => {
-  const isOpen = nav.classList.toggle('open');
-  menuBtn.setAttribute('aria-expanded', isOpen);
-});
