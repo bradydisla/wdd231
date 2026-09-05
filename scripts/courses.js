e26d2f4ebbe467beb308d9c6f8024aaab68443ea
bradydisla <bradydue@gmail.com>
2026-09-05 18:19:37 -0400

    ...

---
+++ scripts/courses.js
@@ -0,0 +1,56 @@
+const courses = [
+  {
+    subject: "CSE",
+    number: 110,
+    title: "Introduction to Programming",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description: "Programming fundamentals using Python.",
+    completed: true
+  },
+  {
+    subject: "WDD",
+    number: 130,
+    title: "Web Fundamentals",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description: "Introduction to HTML and CSS for web development.",
+    completed: true
+  },
+  {
+    subject: "CSE",
+    number: 111,
+    title: "Programming with Functions",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description: "Structured programming using functions.",
+    completed: true
+  },
+  {
+    subject: "CSE",
+    number: 210,
+    title: "Programming with Classes",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description: "Introduction to object-oriented programming.",
+    completed: true
+  },
+  {
+    subject: "WDD",
+    number: 131,
+    title: "Dynamic Web Fundamentals",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description: "Using JavaScript to create dynamic web pages.",
+    completed: true
+  },
+  {
+    subject: "WDD",
+    number: 231,
+    title: "Web Frontend Development I",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description: "Building front-end web applications using JavaScript.",
+    completed: false
+  }
+];
